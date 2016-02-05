64703a09351846cb67dfc5bbc6aedb6f0ba0a5bb
Anton Efremov <antony-efremov@yandex.ru>
2016-02-05 12:02:29 +0300

    ready for merge into master.

--- views/index/main.inc.php
+++ views/index/main.inc.php
@@ -5,5 +5,5 @@ use app\view\RendererInterface;
 /* @var $context RendererInterface */
 
 ?>
-<h1>Simple requester <small>v0.1.0</small></h1>
+<h1>Simple requester <small>v0.1.1</small></h1>
 <?php $context->renderView('form');
